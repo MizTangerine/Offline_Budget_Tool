7b6a77d05ec6c3b1e1ac788f4c4458a9f8fcda1c
Clarissa Reeve <clarissa.reeve@gmail.com>
2020-11-22 13:25:35 -0700

    initial commit from deploy folder

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const logger = require('morgan');
+const mongoose = require('mongoose');
+require('dotenv/config');
+const compression = require('compression');
+
+const PORT = 3000;
+
+const app = express();
+
+app.use(logger('dev'));
+
+app.use(compression());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+// routes
+app.use(require('./routes/api.js'));
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true
+  , useFindAndModify: false
+  , useUnifiedTopology: true
+})
+  .then(() => {
+    app.listen(PORT, () => console.log('==> Listening on port %s. Visit http://localhost:%s/ in your browser.', PORT, PORT));
+  })
+  .catch((error) => { console.error(error.stack); });
